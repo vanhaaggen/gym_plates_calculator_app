e9c3aa7517bd4d85836ad4ab851053ec96068058
vanhaaggen <vanhaaggen@gmail.com>
2020-09-23 12:03:37 +0200

    style mid input section

--- my_plates_app/src/components/BarWeightInput.js
+++ my_plates_app/src/components/BarWeightInput.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import './BarweightInput.sass'
 
 export default function ({ bWeight }) {
     return (
-        <div className="">
-            <div className="">
-                <label htmlFor="targetWeight">Target Weight</label>
-                <input type="number" name="targetWeight" id="targetWeight" />
+        <div className="input-cont">
+            <div className="input-cont__group">
+                <label className="input-cont__group--label" htmlFor="targetWeight">Target Weight</label>
+                <input className="input-cont__group--input" type="number" name="targetWeight" id="targetWeight" />
             </div>
-            <div className="l">
-                <label htmlFor="barWeight">Bar Weight</label>
-                <select name="barWeight" id="barWeight">
+            <div className="input-cont__group">
+                <label className="input-cont__group--label" htmlFor="barWeight">Bar Weight</label>
+                <select className="input-cont__group--input select" name="barWeight" id="barWeight">
                     <option value={`${bWeight[0]}`}>{bWeight[0]}</option>
                     <option value={`${bWeight[1]}`}>{bWeight[1]}</option>
                 </select>
