4e7a2c5498ea4a9c7403176c03defad0b1607060
vanhaaggen <vanhaaggen@gmail.com>
2020-09-27 13:48:56 +0200

    change gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,6 @@ module.exports = {
   },
 
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sass`,
