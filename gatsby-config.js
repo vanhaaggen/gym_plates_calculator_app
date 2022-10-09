97a27aa6f720bb66ea9f9b748167a614b8f7dd0f
vanhaaggen <vanhaaggen@gmail.com>
2022-10-09 22:22:11 +0200

    update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,45 +7,37 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: 'Weight Plates Calulator',
-    description: 'Easy to use Weight Plates Calculator to never rack wrong again your bar',
-    author: 'Christian Haag',
+    title: "Weight Plates Calulator",
+    description:
+      "Easy to use Weight Plates Calculator to never rack wrong again your bar",
+    author: "Christian Haag",
   },
 
   plugins: [
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sass`,
+    `gatsby-plugin-sass
+    gatsby-plugin-preload-fonts`,
+    `gatsby-plugin-preload-fonts`,
     {
-      resolve: `gatsby-plugin-favicon`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        logo: "./src/favicon.png"
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
       },
-      icons: {
-        android: true,
-        appleIcon: true,
-        appleStartup: true,
-        coast: false,
-        favicons: true,
-        firefox: true,
-        yandex: false,
-        windows: false
-      }
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-preload-fonts`,
       options: {
         fonts: [
           {
             family: `Roboto`,
-            variants: [`400`, `700`]
+            variants: [`400`, `700`],
           },
           {
             family: `Open Sans`,
-            variant: [`300`, `400`]
+            variant: [`300`, `400`],
           },
         ],
       },
-    }
+    },
   ],
 }
--- src/helpers/calcWeights.js
+++ src/helpers/calcWeights.js
@@ -1,50 +1,48 @@
-const URL = 'https://whispering-river-40522.herokuapp.com/api' //should be in .ENV file but this is just a test app
-//const URL = 'http://localhost:8080/api'
-const querystring = require('querystring')
+//const URL = "https://whispering-river-40522.herokuapp.com/api" //should be in .ENV file but this is just a test app
+const URL = "http://localhost:8080/api"
+const querystring = require("query-string")
 
 const validate = {
-    number(target, name) {
-        if (target === '') throw new Error(`${name} parameter is empty or blank`)
-        if (typeof target != 'number') throw TypeError(`${name} parameter is not a Number`)
-    },
-    array(target, name) {
-        if (!Array.isArray(target)) throw new Error(`${name} parameter is not an Array`)
-    }
+  number(target, name) {
+    if (target === "") throw new Error(`${name} parameter is empty or blank`)
+    if (typeof target != "number")
+      throw TypeError(`${name} parameter is not a Number`)
+  },
+  array(target, name) {
+    if (!Array.isArray(target))
+      throw new Error(`${name} parameter is not an Array`)
+  },
 }
 
 function calcWeights(desiredWeight, barWeight, weightsAvail, units) {
-    validate.number(desiredWeight, 'desiredWeight')
-    validate.number(barWeight, 'barWeight')
-    validate.array(weightsAvail, 'weightsAvail')
+  validate.number(desiredWeight, "desiredWeight")
+  validate.number(barWeight, "barWeight")
+  validate.array(weightsAvail, "weightsAvail")
 
-    let data = {
-        'dweight': desiredWeight,
-        'bweight': barWeight,
-        'wavail': weightsAvail,
-        'units': units
+  let data = {
+    dweight: desiredWeight,
+    bweight: barWeight,
+    wavail: weightsAvail,
+    units: units,
+  }
+  let xurldata = querystring.stringify({ data: JSON.stringify(data) })
+  return (async () => {
+    const response = await fetch(`${URL}/calcnumplate/${xurldata}`, {
+      method: "GET",
+      //mode: "no-cors",
+      headers: {
+        "Content-Type": "text/JSON",
+        "Access-Control-Request-Method": "GET",
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+
+    if (response.status !== 201) {
+      const { error } = await response.json()
+      throw Error(error)
     }
-    let xurldata = querystring.stringify({ 'data': JSON.stringify(data) })
-    console.log('calcWeights: ', { units, xurldata })
-    return (async () => {
-        const response = await fetch(`${URL}/calcnumplate/${xurldata}`, {
-            method: 'GET',
-            //mode: 'no-cors',
-            headers: {
-                'Content-Type': 'text/JSON',
-                'Access-Control-Request-Method': 'GET',
-                'Access-Control-Allow-Origin': '*'
-            }
-        })
-
-        if (response.status !== 201) {
-            const { error } = await response.json()
-            throw Error(error)
-        }
-
-
-        return response.json()
-
-    })()
+    return response.json()
+  })()
 }
 
 export default calcWeights
